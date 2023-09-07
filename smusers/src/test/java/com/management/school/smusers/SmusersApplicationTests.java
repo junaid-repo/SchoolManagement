7adc971a9100a33bfa88251a9d825d816a948b62
junaid-repo <junaidraza3001@gmail.com>
2023-09-07 12:01:47 +0530

    School management system

---
+++ smusers/src/test/java/com/management/school/smusers/SmusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.school.smusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
